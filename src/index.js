d88c5912627691e0dbfbadb8697adccc4fa954ff
Bronson Dunbar <bronson@bronsondunbar.com>
2019-12-11 16:00:32 +0200

    Updates

--- src/index.js
+++ src/index.js
@@ -1,53 +1,54 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-let links = [
-	{
-  	id: 'link-0',
-  	url: 'www.howtographql.com',
-  	description: 'Fullstack tutorial for GraphQL'
-	}
-]
-let idCount = links.length
+// let links = [
+// 	{
+//   	id: 'link-0',
+//   	url: 'www.howtographql.com',
+//   	description: 'Fullstack tutorial for GraphQL'
+// 	}
+// ]
+// let idCount = links.length
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    allLinks: () => links,
-    getLink: (parent, args) => {
-    	const index = links.findIndex((link => link.id === args.id))
-    	return links[index]
-    }
+    allLinks: (root, args, context, info) => {
+      return context.prisma.links()
+    },
+    // getLink: (parent, args) => {
+    // 	const index = links.findIndex((link => link.id === args.id))
+    // 	return links[index]
+    // }
   },
   Mutation: {
-    createLink: (parent, args) => {
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
+    createLink: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      }
-      links.push(link)
-      return link
+        description: args.description,
+      })
     },
-    updateLink: (parent, args) => {
-    	const index = links.findIndex((link => link.id === args.id))
+   //  updateLink: (parent, args) => {
+   //  	const index = links.findIndex((link => link.id === args.id))
 
-			links[index].url = args.url ? args.url : links[index].url
-			links[index].description = args.description ? args.description : links[index].description
+			// links[index].url = args.url ? args.url : links[index].url
+			// links[index].description = args.description ? args.description : links[index].description
 
-    	return links[index]
-    },
-    deleteLink: (parent, args) => {
-    	const index = links.findIndex((link => link.id === args.id))
+   //  	return links[index]
+   //  },
+   //  deleteLink: (parent, args) => {
+   //  	const index = links.findIndex((link => link.id === args.id))
 
-    	links.splice(index, 1)
+   //  	links.splice(index, 1)
 
-    	return links
-    }
+   //  	return links
+   //  }
   },
 }
 
 const server = new GraphQLServer({
   typeDefs : './src/schema.graphql',
   resolvers,
+  context: { prisma },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
