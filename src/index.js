a2f366fc421697892b4ee11b48e2f45d6fc6a9c8
Bronson Dunbar <bronson@bronsondunbar.com>
2019-12-11 16:32:15 +0200

    Updates

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
 
-// let links = [
-// 	{
-//   	id: 'link-0',
-//   	url: 'www.howtographql.com',
-//   	description: 'Fullstack tutorial for GraphQL'
-// 	}
-// ]
-// let idCount = links.length
-
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     allLinks: (root, args, context, info) => {
       return context.prisma.links()
     },
-    // getLink: (parent, args) => {
-    // 	const index = links.findIndex((link => link.id === args.id))
-    // 	return links[index]
-    // }
   },
   Mutation: {
     createLink: (root, args, context) => {
