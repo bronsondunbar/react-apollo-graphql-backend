d7d911c5ada5dd2672faec54de51fd8251ca3e4f
Bronson Dunbar <bronson@bronsondunbar.com>
2020-01-06 12:27:10 +0200

    Updates

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,19 @@
-function allLinks(root, args, context, info) {
-  return context.prisma.links()
+async function allLinks(root, args, context, info) {
+  const where = args.filter ? {
+    OR: [
+      { description_contains: args.filter },
+      { url_contains: args.filter },
+    ],
+  } : {}
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy
+  })
+
+  return links
 }
 
 module.exports = {
